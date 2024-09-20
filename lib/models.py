5b667de70d427602c1fb4982f9ecced6bc684d68
toel-kamanda34 <toelokemwa@gmail.com>
2024-09-20 06:51:13 +0300

    completed code challenge

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey, Column, Integer,Boolean, String, MetaData, create_engine
+from sqlalchemy.orm import relationship,sessionmaker,  backref
+from sqlalchemy.orm import declarative_base
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,3 +8,54 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer(), primary_key=True)
+    actor = Column(String())
+    location = Column(String())
+    phone = Column(Integer())
+    hired = Column(Boolean, default=False)
+    role_id = Column(Integer(), ForeignKey('roles.id'))
+    
+    engine = create_engine('sqlite:///theater.db')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    def __repr__(self):
+        return f'Audition(id={self.id}, ' + \
+            f'actor={self.actor}, ' + \
+            f'role_id={self.role_id})'
+    
+    def call_back(self):
+        self.hired = True
+    
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer(), primary_key=True)
+    character_name = Column(String())
+
+    def __repr__(self):
+        return f'Role(id={self.id}, ' + \
+            f'character_name={self.character_name})'    
+    
+    auditions = relationship('Audition', backref='role')
+
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+    
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+   
+    def lead(self):
+        lead_audition = next((audition for audition in self.auditions if audition.hired), None)
+        return lead_audition or 'no actor has been hired for this role'
+    
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        return hired_auditions[1] if len(hired_auditions) > 1 else 'no actor has been hired for understudy for this role'
+    
